0534127633ef08786186715e5c48ac3757850e1a
Jangsito <113041492+Jangsito@users.noreply.github.com>
2022-10-27 16:40:44 -0500

    Create sumavario.cpp

---
+++ JulioMerelo/sumavario.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+ using namespace std;                                                                                                                                            //creadoporPaulobaColobon
+
+int main()
+
+  {
+float JAMG_x,JAMG_s=0.0;
+
+int JAMG_i=0,JAMG_l;
+
+cout<<"Ingrese el valor de JAMG_l: ";                                                                                                                                            cin>>CBPJ_l;                                                      
+do{
+
+cout<<"Ingrese el valor de JAMG_x: ";
+
+cin>>JAMG_x;
+
+JAMG_i=JAMG_i+1;
+
+JAMG_s=JAMG_s+JAMG_x;
+
+}while(JAMG_i<JAMG_l);                                            
+cout<<"La suma de los número fue: "<<JAMG_s<<endl;                                                                                                                  return 0;
+
+}
